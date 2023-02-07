44047d05400bd05ef9c084c4555c67dddb2eb2a5
Casper Cromjongh <casper.cromjongh@hotmail.com>
2023-02-07 18:37:52 +0100

    Make readings more precise.

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,17 @@
 #include <ArduinoJson.h>
 #include "Streaming.h"
 
-const double ANALOG_MAX = 1023;
+const double ANALOG_MAX = 4095;
 const double NOM_VOLTAGE = 3.3;
 const double scaler = NOM_VOLTAGE/ANALOG_MAX;
 uint8_t voltage_pins[] = {26, 27};
 const double voltage_conversion[] = {7.8/1.0 * scaler, 1.0 * scaler};
 uint8_t current_pins[] = {28};
 const double current_conversion[] = {1680.0/1000.0 * scaler / 0.185};
-const double current_bias[] = {-2.456 / 0.185};
+const double current_bias[] = {-2.51 / 0.185};
+
+uint32_t voltage_readings[2] = {0};
+uint32_t current_readings[2] = {0};
 
 // the setup routine runs once when you press reset:
 void setup() {
@@ -23,6 +26,7 @@ void setup() {
     for (auto pin: current_pins) {
         pinMode(pin, INPUT);
     }
+    analogReadResolution(12);  // Set analog resolution to the full 12 bits.
     Serial.println("Finish set-up");
 }
 
@@ -32,17 +36,28 @@ void loop() {
     JsonArray voltages = doc.createNestedArray("voltages");
     JsonArray currents = doc.createNestedArray("currents");
 
+    memset(static_cast<void *>(voltage_readings), 0, sizeof(voltage_readings));
+    memset(static_cast<void *>(current_readings), 0, sizeof(current_readings));
+
+    const int AVERAGE = 15;
+    for (int l = 0; l < AVERAGE; ++l) {
+        for (unsigned int i = 0; i < sizeof(voltage_pins); ++i) {
+            voltage_readings[i] += analogRead(voltage_pins[i]);
+        }
+        for (unsigned int i = 0; i < sizeof(current_pins); ++i) {
+            current_readings[i] += analogRead(current_pins[i]);
+        }
+        delay(20);
+    }
+
     digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    delay(200);               // wait for half a second
 
     for (unsigned int i = 0; i < sizeof(voltage_pins); ++i) {
-        int int_value = analogRead(voltage_pins[i]);
-        double value = voltage_conversion[i] * static_cast<double>(int_value);
+        double value = voltage_conversion[i] * (static_cast<double>(voltage_readings[i]) / static_cast<double>(AVERAGE));
         voltages.add(value);
     }
     for (unsigned int i = 0; i < sizeof(current_pins); ++i) {
-        int int_value = analogRead(current_pins[i]);
-        double value = current_conversion[i] * static_cast<double>(int_value) + current_bias[i];
+        double value = current_conversion[i] * (static_cast<double>(current_readings[i]) / static_cast<double>(AVERAGE)) + current_bias[i];
         currents.add(value);
     }
     serializeJson(doc, Serial);
